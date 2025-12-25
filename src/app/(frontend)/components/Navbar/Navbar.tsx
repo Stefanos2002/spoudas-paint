75ed53f351808fb08b15a0d97c083951d8ee5d8e
Stefanos2002 <kalouliss@gmail.com>
2025-12-25 13:06:23 +0200

    added the logo

--- src/app/(frontend)/components/Navbar/Navbar.tsx
+++ src/app/(frontend)/components/Navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import { GoPersonFill } from 'react-icons/go'
 import { IoIosCall } from 'react-icons/io'
 import { FaPaintRoller } from 'react-icons/fa'
 import { FaImages } from 'react-icons/fa'
+import Image from 'next/image'
 
 export default function Navbar() {
   const pathname = usePathname()
@@ -18,7 +19,16 @@ export default function Navbar() {
     { href: '#επικοινωνια', label: 'Καλέστε μας', icon: <IoIosCall /> },
   ]
   return (
-    <nav id="navbar" className="fixed z-30 bg-blue-950 items-center justify-center h-22 w-full">
+    <nav id="navbar" className="fixed z-30 bg-blue-950 flex justify-between h-22 w-full">
+      <div id="logo" className="w-max flex">
+        <Image src="/images/logo-splash.png" alt="logo-splash" width={100} height={100} />
+        <div className="flex flex-col -translate-x-10 justify-center">
+          <h1 className="text-white text-3xl font-black tracking-tighter leading-none">ΣΠΟΥΔΑΣ</h1>
+          <span className="text-blue-100 bg-blue-700 p-1 text-xs font-semibold tracking-wider uppercase mt-1">
+            ΧΡΩΜΑ & ΔΙΑΚΟΣΜΗΣΗ
+          </span>
+        </div>
+      </div>
       <ul
         className={`text-neutral-300  ${styles.paint} text-lg flex gap-6 flex-row justify-end items-center mr-12 h-full`}
       >
