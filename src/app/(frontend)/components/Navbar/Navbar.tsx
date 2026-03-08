f10361b02dea22261828a6c29449d81359d3d271
Stefanos2002 <kalouliss@gmail.com>
2026-03-08 17:53:58 +0200

    .

--- src/app/(frontend)/components/Navbar/Navbar.tsx
+++ src/app/(frontend)/components/Navbar/Navbar.tsx
@@ -1,19 +1,24 @@
 import Image from 'next/image'
 import DesktopMenu from '../DesktopMenu/DekstopMenu'
 import MobileMenu from '../MobileMenu/MobileMenu'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
     <nav id="navbar" className="fixed z-30 bg-blue-950 flex justify-between h-19 w-full">
-      <div id="logo" className="w-max flex">
-        <Image src="/images/logo-splash.png" alt="logo-splash" width={100} height={100} />
-        <div className="flex flex-col -translate-x-10 justify-center">
-          <h1 className="text-white text-3xl font-black tracking-tighter leading-none">ΣΠΟΥΔΑΣ</h1>
-          <span className="text-blue-100 text-center bg-blue-700 p-1 text-xs font-semibold tracking-wider uppercase mt-1">
-            ΧΡΩΜΑ & ΔΙΑΚΟΣΜΗΣΗ
-          </span>
+      <Link href="/#αρχική">
+        <div id="logo" className="w-max flex">
+          <Image src="/images/logo-splash.png" alt="logo-splash" width={100} height={100} />
+          <div className="flex flex-col -translate-x-10 justify-center">
+            <h1 className="text-white text-3xl font-black tracking-tighter leading-none">
+              ΣΠΟΥΔΑΣ
+            </h1>
+            <span className="text-blue-100 text-center bg-blue-700 p-1 text-xs font-semibold tracking-wider uppercase mt-1">
+              ΧΡΩΜΑ & ΔΙΑΚΟΣΜΗΣΗ
+            </span>
+          </div>
         </div>
-      </div>
+      </Link>
       <DesktopMenu />
       <MobileMenu />
     </nav>
